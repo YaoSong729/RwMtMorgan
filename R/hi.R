b84bde824a8fbc108ace0c68d3f1ee0e4c81962f
Yao <yao@c02jr0v9dr55.roswellpark.org>
2016-10-05 17:42:09 -0400

    implement "how" for hi() function
    add shout for upper cases
    
    add whisper for lower cases

--- R/hi.R
+++ R/hi.R
@@ -5,17 +5,30 @@
 #'
 #' @param who character(1) The name of the person you wish to start a
 #'     conversation with.
+#'
+#' @param how character(1) How to greet the convertant
 #' @return character(1) A line of text to be used when starting conversations.
 #'
 #' @examples
 #' hi("Song Yao")
+#' hi("Song Yao", "whisper")
 #'
 #' @export
-hi <- function(who) {
-    stopifnot(is.character(who), length(who) == 1, !is.na(who))
-    paste("hello", who, "you have", nchar(who), "letters in your name")
+hi <- function(who, how = c("shout", "whisper")) {
+    stopifnot(
+        is.character(who),
+        length(who) == 1,
+        !is.na(who)
+    )
+    how <- match.arg(how)
+    fun = switch(how, shout = shout, whisper = whisper)
+    paste("hello", fun(who), "you have", nchar(who), "letters in your name")
 }
 
 shout <- function(who) {
   toupper(who)
+}
+
+whisper <- function(who) {
+    tolower(who)
 }
