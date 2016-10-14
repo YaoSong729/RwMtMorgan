48b251f14db04dbe42e3848199b39087842c995a
Yao <yao@c02jr0v9dr55.roswellpark.org>
2016-10-14 17:05:42 -0400

    implement unit tests
    - using testthat package
    - for is_scalar_character

--- R/utilities.R
+++ R/utilities.R
@@ -1,5 +1,5 @@
 #' utility functions not to be exported
 
 is_scalar_character <- function(x) {
-    is.character(x) && length(x) == 1 && !is.na(x)
+    is.character(x) && length(x) == 1 && !is.na(x) && nzchar(x)
 }
